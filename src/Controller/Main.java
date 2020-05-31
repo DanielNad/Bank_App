227e99326c01a6e25d77f778861b8d4133d13d2a
DanielNadav232 <54536436+DanielNadav232@users.noreply.github.com>
2020-05-31 17:21:41 +0300

    Bank_App

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -3,7 +3,8 @@ package Controller;
 import Model.Client;
 import Model.User;
 import View.ViewApp;
-
+//TODO: Create tables if there are no existing ones already
+//TODO: Error handling - transferring to an invalid client id etc.
 public class Main {
     public static void main(String[] args) {
         ViewApp panel = new ViewApp();
